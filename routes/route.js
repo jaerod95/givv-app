51ab140070d199e5553ae819923f826f1d1846bb
Jordin Gardner <gard.jordin@gmail.com>
2017-10-13 21:44:10 -0700

    uhh

--- routes/route.js
+++ routes/route.js
@@ -1,18 +1,17 @@
 import { StackNavigator } from "react-navigation";
 
-import HomeView from '../views/HomeView/HomeView';
-import SelectionView from '../views/SelectionView/SelectionView';
-import RootViewAdmin from '../views/RootViewAdmin/RootViewAdmin';
-import RootViewBusiness from '../views/RootViewBusiness/RootViewBusiness';
-import RootViewEmployee from '../views/RootViewEmployee/RootViewEmployee';
+import HomeView from "../views/HomeView/HomeView";
+// import SelectionView from "../views/SelectionView/SelectionView";
+// import RootViewAdmin from "../views/RootViewAdmin/RootViewAdmin";
+// import RootViewBusiness from "../views/RootViewBusiness/RootViewBusiness";
+// import RootViewEmployee from "../views/RootViewEmployee/RootViewEmployee";
 
 export const SimpleApp = StackNavigator({
-
-  Home: { screen: HomeView }, // login screen
-  Selection: { screen: SelectionView },
-  RootViewAdmin: { screen: RootViewAdmin },
-  RootViewBusiness: { screen: RootViewBusiness },
-  RootViewEmployee: { screen: RootViewEmployee }
+  Home: { screen: HomeView } // login screen
+  // Selection: { screen: SelectionView },
+  // RootViewAdmin: { screen: RootViewAdmin },
+  // RootViewBusiness: { screen: RootViewBusiness },
+  // RootViewEmployee: { screen: RootViewEmployee }
 });
 
 import route from "./route";
